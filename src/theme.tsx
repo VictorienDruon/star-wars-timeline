e6327bc1dc19e017a6d50263191ede3c1479017b
Constant Druon <druon.constant@gmail.com>
2022-08-08 23:44:51 +0200

    fix: color mode issue with Next js

--- src/theme.tsx
+++ src/theme.tsx
@@ -2,22 +2,8 @@ import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   config: {
-    initialColorMode: "dark",
     useSystemColorMode: false,
-  },
-  components: {
-    Heading: {
-      defaultProps: {
-        size: "2xl",
-        letterSpacing: "tight",
-        fontWeight: "extrabold",
-      },
-    },
-    Link: {
-      baseStyle: ({ colorMode }: { colorMode: any }) => ({
-        color: colorMode === "light" ? "blue.500" : "blue.200",
-      }),
-    },
+    initialColorMode: "dark",
   },
 });
 
