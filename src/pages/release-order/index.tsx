6c4c71ac3be1e07843754625a7cfff703ee4c1e7
VictorienDruon <victoriendruon@icloud.com>
2022-09-03 09:54:55 +0200

    fix : release order sort function

--- src/pages/release-order/index.tsx
+++ src/pages/release-order/index.tsx
@@ -2,7 +2,9 @@ import type { NextPage } from "next";
 import { Template } from "src/components/Template";
 import { movieList } from "utils/movieList";
 
-const releaseOrder = [...movieList].sort((a, b) => a.release - b.release);
+const releaseOrder = [...movieList].sort(
+  (a, b) => parseInt(a.release.slice(0, 4), 10) - parseInt(b.release.slice(0, 4), 10),
+);
 
 const ReleaseOrder: NextPage = () => {
   return <Template movieList={releaseOrder} />;
